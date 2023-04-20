1ba6d82a6190a606477ddca05096c6abad7aac72
Timz Mini <timz_mini@Timzs-Mac-mini.local>
2023-04-20 13:21:16 -0700

    testing on netlify 1

--- src/api/todoDataService.ts
+++ src/api/todoDataService.ts
@@ -2,20 +2,30 @@ import Todo from '../models/todo';
 import axios from 'axios';
 
 const getTodos = async () => {
-  const todos = await axios.get('http://localhost:3010/todos');
+  const todos = await axios.get(`${process.env.REACT_APP_LOCAL_HOST}/todos`);
   return todos;
 };
 
+const nfyTest = async () => {
+  const testPacket = await axios.get(
+    `${process.env.REACT_APP_NETLIFY_HOST}/test`
+  );
+  console.log(testPacket.data);
+  return testPacket;
+};
+
 const createTodo = async (todo: Todo) => {
-  return await axios.post('http://localhost:3010/todos', todo);
+  return await axios.post(`${process.env.REACT_APP_LOCAL_HOST}/todos`, todo);
 };
 
 const updateTodo = async (todo: Todo) => {
-  return await axios.put('http://localhost:3010/todos', todo);
+  return await axios.put(`${process.env.REACT_APP_LOCAL_HOST}/todos`, todo);
 };
 
 const deleteTodo = async (todo: Todo) => {
-  return await axios.delete(`http://localhost:3010/todos/${todo.id}`);
+  return await axios.delete(
+    `${process.env.REACT_APP_LOCAL_HOST}/todos/${todo.id}`
+  );
 };
 
 const TodoDataService = {
@@ -23,6 +33,7 @@ const TodoDataService = {
   createTodo,
   updateTodo,
   deleteTodo,
+  nfyTest,
 };
 
 export default TodoDataService;
